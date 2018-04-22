9935257e9a76ad94503aa108596d1a35e1309b90
Charlton Smith <charltonuw@gmail.com>
2018-04-22 07:59:53 -0700

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,11 +6,6 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey:'AIzaSyDfUCfAqI6pA1AHThlRFoeMN5GSdAgLP7E',
-    authDomain:'benchme-df59a.firebaseapp.com',
-    databaseURL:'https://benchme-df59a.firebaseio.com/',
-    projectId:'benchme-df59a',
-    storageBucket: 'benchme-df59a.appspot.com',
-    messangingSenderId:'436734107308'
+ASK FOR API KEY
   }
 };
